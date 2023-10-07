20a04067b5d2ccb4cdb22676f54ca7e5aae812fe
vrnsky <vrnsky@protonmail.ch>
2023-10-07 18:13:44 +0800

    Initial commit with stub implmentation

---
+++ src/main/java/my/edu/sunway/wbrms/wbrmsmetricsservice/WbrmsMetricsServiceApplication.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsmetricsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbrmsMetricsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbrmsMetricsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/edu/sunway/wbrms/wbrmsmetricsservice/WbrmsMetricsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsmetricsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbrmsMetricsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
